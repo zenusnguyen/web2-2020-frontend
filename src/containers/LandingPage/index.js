ac96d875599bc583eeb02fd5be0182ba5d4e4143
zenusnguyen <zenusnguyen@gmail.com>
2020-08-02 00:27:37 +0700

    add langding page

--- src/containers/LandingPage/index.js
+++ src/containers/LandingPage/index.js
@@ -12,7 +12,7 @@ import Savings from "../../assets/save-acc.svg";
 export default function LandingPage() {
   return (
     <LandingPageStyle>
-      <Header Height="100px" Shadow="0" CTA="flex" Href="/"></Header>
+      <Header Height="100px" Shadow="0" CTA="flex" Href="/signin"></Header>
       <div style={{ maxWidth: "1440px", width: "100%" }}>
         <div className="hero">
           <h1 style={{ marginTop: "80px", width: "838px", height: "200px" }}>
@@ -24,7 +24,9 @@ export default function LandingPage() {
         </div>
 
         <div style={{ textAlign: "center" }}>
+          <a href="/register">
           <button className="CTAbutton">Join now</button>
+          </a>
         </div>
 
         <div style={{ textAlign: "center" }}>
