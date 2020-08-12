ab3097c3c013285348305f348578483d49bd6ede
zenusnguyen <zenusnguyen@gmail.com>
2020-08-12 16:05:07 +0700

    update

--- src/containers/AllCustomerPage/index.js
+++ src/containers/AllCustomerPage/index.js
@@ -8,17 +8,19 @@ import CustomerCard from "../../components/CustomerCard";
 import axios from "axios";
 import CustomerDetailPage from "../CustomerDetailPage";
 import { config } from "../../configs/server";
+import * as _ from "lodash";
 export default function MaganeAccount() {
-  const temp = [];
-  const [data, setData] = useState(temp);
-
+  const [customer, setCustomer] = useState([]);
+  const [customerRender, setCustomerRender] = useState([]);
   const [isDetail, setIsDetail] = useState(false);
   const [styled, setStyled] = useState("");
   const [styled2, setStyled2] = useState("none");
   const [cardInfo, setcardInfo] = useState("");
   const [state, setState] = useState("detail");
+  const [spendAccount, setSpendAccount] = useState([]);
+  const [searchValeu, setSearchValue] = useState("");
   useEffect(() => {
-    async function Fecth() {
+    async function FecthUser() {
       const result = await axios.get(
         `${config.server}/users-permissions/users-active`,
         {
@@ -27,18 +29,79 @@ export default function MaganeAccount() {
           },
         }
       );
-      setData(result.data);
+      setCustomer(result.data);
+      setCustomerRender(result.data);
     }
-    Fecth();
+    FecthUser();
+    async function FecthSpend() {
+      const result = await axios.get(`${config.server}/spend-accounts`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setSpendAccount(result.data);
+    }
+    FecthSpend();
   }, []);
-
+  // console.log("customer: ", customer);
+  // console.log("spendAccount: ", spendAccount);
   const HandlerClick = (items) => {
     setcardInfo(items);
     setState("card");
   };
 
+  const filterUserName = (text) => {
+    let data = [];
+
+    _.forEach(customer, (item) => {
+      if (item.username == text) {
+        data.push(item);
+      }
+    });
+    return data;
+  };
+
+  const filterEmail = (text) => {
+    let data = [];
+
+    _.forEach(customer, (item) => {
+      if (item.email == text) {
+        data.push(item);
+      }
+    });
+    return data;
+  };
+
+  const filterIDCard = (text) => {
+    let data = [];
+
+    _.forEach(spendAccount, (item) => {
+      if (item.card_number == text) {
+        _.forEach(customer, (item2) => {
+          if (item2.id == item.account_id) {
+            data.push(item2);
+          }
+        });
+      }
+    });
+    return data;
+  };
+  const handlerSearch = (e) => {
+    let result = [];
+    if (filterEmail(e).length !== 0) {
+      result = filterEmail(e);
+    } else if (filterUserName(e).length !== 0) {
+      result = filterUserName(e);
+    } else if (filterIDCard(e).length !== 0) {
+      result = filterIDCard(e);
+    } else if (e.length == 0) {
+      result = customer;
+    }
+    setCustomerRender(result);
+  };
+
   const RenderCard = () => {
-    return data.map((items, index) => (
+    return customerRender.map((items, index) => (
       <CustomerCard
         onClick={() => {
           HandlerClick(items);
@@ -60,7 +123,13 @@ export default function MaganeAccount() {
             <p className="SignInTitle"> All customers </p>
             <div className="searchBar">
               <img src={Search}></img>
-              <input placeholder="Search" type="text"></input>
+              <input
+                placeholder="Search"
+                type="text"
+                onChange={(e) => {
+                  handlerSearch(e.target.value);
+                }}
+              ></input>
             </div>
           </div>
           <div className="listCard">
