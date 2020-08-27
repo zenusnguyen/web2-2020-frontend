f4cc10d532a0ad223d255e90c18c95313375b9cf
Tuan <anhtuanhoang2312@gmail.com>
2020-08-27 15:59:45 +0700

    fixed status label

--- src/components/CustomerCard/styled.js
+++ src/components/CustomerCard/styled.js
@@ -4,12 +4,12 @@ const CardStyled = styled.div`
   background-color: #ffffff;
   display: flex;
   flex-direction: row;
+  width: 100%;
   padding: 16px 32px;
   border-radius: 16px;
   margin-bottom: 16px;
   align-items: center;
   box-shadow: 0px 4px 16px rgba(35, 35, 35, 0.06);
-  min-width: 700px;
   .icon {
     width: 32px;
     height: 32px;
@@ -19,17 +19,18 @@ const CardStyled = styled.div`
     }
   }
 
+  p {
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 150%;
+    margin-bottom: 0;
+    color: #333435;
+  }
+
   .detail-left {
     margin-left: 24px;
     display: flex;
     flex-direction: column;
-    p {
-      font-size: 20px;
-      font-weight: 500;
-      line-height: 150%;
-      margin-bottom: 0;
-      color: #333435;
-    }
   }
 
   .detail-right {
@@ -37,7 +38,6 @@ const CardStyled = styled.div`
     flex-direction: column;
     text-align: right;
     margin-left: auto;
-    font-size: 16px;
   }
 
   p.subtext {
@@ -49,21 +49,19 @@ const CardStyled = styled.div`
   .withStatus {
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
     align-items: center;
-    margin-right: auto;
   }
   .status {
-    width: 92px;
+    width: 60px;
     height: 24px;
-    background: "yellow";
+    background: #56cd67;
     border-radius: 4px;
     display: flex;
     justify-content: center;
     align-items: center;
     margin-left: 12px;
     p {
-      margin: 0;
+      margin: auto;
       font-weight: normal;
       font-size: 12px;
       line-height: 130%;
